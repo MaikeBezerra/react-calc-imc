051c69ed820153b30276fd4128dd459017be41f6
MaikeBezerra <maike_ti@hotmail.com>
2024-02-22 15:09:46 -0300

    create: initial page version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './asserts/powered.png';
 
 function App() {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+
+    } else {
+      alert("Digite todos os campos")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt='' width={150}></img>
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calucule seu IMC.</h1>
+          <p>
+            IMC é a sigla para Indice de massa corpórea, parametro adotado
+            pela Organização mudial de saude para calcular o peso ideal 
+            de cada pessoa.
+          </p>
+          <input 
+            type='number' 
+            placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          <input 
+            type='number' 
+            placeholder='Digite o seu peso. Ex: 75.5 (em kg)'
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+
+        </div>
+      </div>
     </div>
   );
 }
